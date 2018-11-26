057689c759b4ae4838c44b423f39c5eb2a12986d
Danner36 <jareddanner3@gmail.com>
2018-11-26 14:44:42 -0600

    Corrected Pin assignments for FMCP_HSPC_LA connectors. Removed unneeded FMCP_HSPC_HA connectors leftover from kcu105 implementation. Added SFP_SI5283 ports. Addeded FMCP_HSPC_GBTCLK & M2C CLK connections.

--- migen/build/platforms/zcu111.py
+++ migen/build/platforms/zcu111.py
@@ -220,290 +220,215 @@ _io = [
     #    Subsignal("n", Pins("P1"))
     #),
 
-    # SFP0_TX_(N/P)
-    ("sfp_tx", 0,
-        Subsignal("p", Pins("Y35")),
-        Subsignal("n", Pins("Y36"))
-    ),
     # SFP0_RX_(N/P)
     ("sfp_rx", 0,
         Subsignal("p", Pins("AA38")),
         Subsignal("n", Pins("AA39"))
     ),
-    # SFP0_TX_DISABLE_B
-    ("sfp_tx_disable_n", 0, Pins("G12"), IOStandard("LVCMOS18")),
-
-    # SFP1_TX_(N/P)
-    ("sfp_tx", 1,
-        Subsignal("p", Pins("V35")),
-        Subsignal("n", Pins("V36"))
-    ),
-    # SFP1_RX_(N/P)
     ("sfp_rx", 1,
         Subsignal("p", Pins("W38")),
         Subsignal("n", Pins("W39"))
     ),
-    # SFP1_TX_DISABLE_B
-    ("sfp_tx_disable_n", 1, Pins("G10"), IOStandard("LVCMOS18")),
-
-    # SFP2_TX_(N/P)
-    ("sfp_tx", 2,
-        Subsignal("p", Pins("T35")),
-        Subsignal("n", Pins("T36"))
-    ),
-    # SFP2_RX_(N/P)
     ("sfp_rx", 2,
         Subsignal("p", Pins("U38")),
         Subsignal("n", Pins("U39"))
     ),
-    # SFP2_TX_DISABLE_B
-    ("sfp_tx_disable_n", 2, Pins("K12"), IOStandard("LVCMOS18")),
-
-    # SFP3_TX_(N/P)
-    ("sfp_tx", 3,
-        Subsignal("p", Pins("R33")),
-        Subsignal("n", Pins("R34"))
-    ),
-    # SFP3_RX_(N/P)
     ("sfp_rx", 3,
         Subsignal("p", Pins("R38")),
         Subsignal("n", Pins("R39"))
     ),
-    # SFP3_TX_DISABLE_B
-    ("sfp_tx_disable_n", 3, Pins("J7"), IOStandard("LVCMOS18")),
-]
 
+    # SFP0_TX_(N/P)
+    ("sfp_tx", 0,
+        Subsignal("p", Pins("Y35")),
+        Subsignal("n", Pins("Y36"))
+    ),
+    ("sfp_tx", 1,
+        Subsignal("p", Pins("V35")),
+        Subsignal("n", Pins("V36"))
+    ),
+    ("sfp_tx", 2,
+        Subsignal("p", Pins("T35")),
+        Subsignal("n", Pins("T36"))
+    ),
+    ("sfp_tx", 3,
+        Subsignal("p", Pins("R33")),
+        Subsignal("n", Pins("R34"))
+    ),
+
+    # SFP0_TX_DISABLE_B
+    ("sfp_tx_disable_n", 0, Pins("G12"), IOStandard("LVCMOS18")),
+    ("sfp_tx_disable_n", 1, Pins("G10"), IOStandard("LVCMOS18")),
+    ("sfp_tx_disable_n", 2, Pins("K12"), IOStandard("LVCMOS18")),
+    ("sfp_tx_disable_n", 3, Pins("J7"), IOStandard("LVCMOS18")),
+    
+    # SFP_REC_CLOCK_C_(N/P)
+    ("sfp_rec_clock_c", 0,
+    	Subsignal("p", Pins("AW14")),
+    	Subsignal("n", Pins("AW13")),
+    	IOStandard("LVDS")
+	),
+
+	# SFP_SI5382_IN1_C_(N/P)
+    ("sfp_si5382_in1_c", 0,
+    	Subsignal("p", Pins("AA33")),
+    	Subsignal("n", Pins("AA34"))
+	),
+
+	# SFP_SI5382_OUT_C_(N/P)
+    ("sfp_si5382_out_c", 0,
+    	Subsignal("p", Pins("Y31")),
+    	Subsignal("n", Pins("Y32"))
+	),
+
+	# SFP_SI5382_CLK_IN_SEL
+    ("sfp_si5382_in1_c", 0, Pins("E39"), IOStandard("LVCMOS18")),
+
+]
 
 _connectors = [
     ("FMCP_HSPC", {
-    	# FMCP_HSPC_DP0_
+
+    	# FMCP_HSPC_DP(#)_
         "DP0_C2M_P": "P35",
         "DP0_C2M_N": "P36",
         "DP0_M2C_P": "N38",
         "DP0_M2C_N": "N39",
-        # FMCP_HSPC_DP1_
         "DP1_C2M_P": "N33",
         "DP1_C2M_N": "N34",
         "DP1_M2C_P": "M36",
         "DP1_M2C_N": "M37",
-        # FMCP_HSPC_DP2_
         "DP2_C2M_P": "L33",
         "DP2_C2M_N": "L34",
         "DP2_M2C_P": "L38",
         "DP2_M2C_N": "L39",
-        # FMCP_HSPC_DP3_
         "DP3_C2M_P": "B6",
         "DP3_C2M_N": "B5",
         "DP3_M2C_P": "A4",
         "DP3_M2C_N": "A3",
-        # FMCP_HSPC_DP4_
         "DP4_C2M_P": "N4",
         "DP4_C2M_N": "N3",
         "DP4_M2C_P": "M2",
         "DP4_M2C_N": "M1",
-        # FMCP_HSPC_DP5_
         "DP5_C2M_P": "J4",
         "DP5_C2M_N": "J3",
         "DP5_M2C_P": "H2",
         "DP5_M2C_N": "H1",
-        # FMCP_HSPC_DP6_
         "DP6_C2M_P": "L4",
         "DP6_C2M_N": "L3",
         "DP6_M2C_P": "K2",
         "DP6_M2C_N": "K1",
-        # FMCP_HSPC_DP7_
         "DP7_C2M_P": "G4",
         "DP7_C2M_N": "G3",
         "DP7_M2C_P": "F2",
         "DP7_M2C_N": "F1",
-        # FMCP_HSPC_DP8_
         "DP8_C2M_P": "D31",
         "DP8_C2M_N": "D32",
         "DP8_M2C_P": "E38",
         "DP8_M2C_N": "E39",
-        # FMCP_HSPC_DP9_
         "DP9_C2M_P": "C33",
         "DP9_C2M_N": "C34",
         "DP9_M2C_P": "D36",
         "DP9_M2C_N": "D37",
-        # FMCP_HSPC_DP10_
         "DP10_C2M_P": "B31",
         "DP10_C2M_N": "B32",
         "DP10_M2C_P": "C38",
         "DP10_M2C_N": "C39",
-        # FMCP_HSPC_DP11_
         "DP11_C2M_P": "A33",
         "DP11_C2M_N": "A34",
         "DP11_M2C_P": "B36",
         "DP11_M2C_N": "B37",
-        # FMCP_HSPC_GBTCLK0_M2C_C_(N/P)
+
+        # FMCP_HSPC_GBTCLK0_M2C_(N/P)
         "GBTCLK0_M2C_C_N": "W34",
         "GBTCLK0_M2C_C_P": "W33",
+        "GBTCLK1_M2C_C_N": "U34",
+        "GBTCLK1_M2C_C_P": "U33",
+        "GBTCLK2_M2C_C_N": "P32",
+        "GBTCLK2_M2C_C_P": "P31",
+
+        # FMCP_HSPC_CLK(#)_M2C_(M/P)
+        "CLK0_M2C_P": "AN10",
+        "CLK0_M2C_N": "AP10",
+        "CLK1_M2C_P": "AP20",
+        "CLK1_M2C_N": "AP19",
+
         # FMCP_HSPC_LA(##)_(N/P)
-        "LA33_P": "AG18",
-        "LA33_N": "AH18",
-
-        "LA32_P": "AF20",
-        "LA32_N": "AF19",
-        
-        "LA31_P": "AJ20",
-        "LA31_N": "AJ19",
-        
-        "LA30_P": "AG20",
-        "LA30_N": "AH20",
-        
-        "LA29_P": "AK22",
-        "LA29_N": "AK21",
-        
-        "LA28_P": "AJ18",
-        "LA28_N": "AK18",
-        
-        "LA27_P": "AR21",
-        "LA27_N": "AT21",
-        
-        "LA26_P": "AR22",
-        "LA26_N": "AT22",
-        
-        "LA25_P": "AL21",
-        "LA25_N": "AL20",
-        
-        "LA24_P": "AL22",
-        "LA24_N": "AM22",
-        
-        "LA23_P": "AM18",
-        "LA23_N": "AN18",
-        
-        "LA22_P": "AR19",
-        "LA22_N": "AT19",
-        
-        "LA21_P": "AL19",
-        "LA21_N": "AM19",
-        
-        "LA20_P": "AR17",
-        "LA20_N": "AT17",
-        
-        "LA19_P": "AU20",
-        "LA19_N": "AU19",
-        
-        "LA18_P": "AM20",
-        "LA18_N": "AN20",
-        
-        "LA17_P": "AN21",
-        "LA17_N": "AP21",
-        
-        "LA16_P": "AR12",
-        "LA16_N": "AR11",
-        
-        "LA15_P": "AJ14",
-        "LA15_N": "AK14",
-        
-        "LA14_P": "AL14",
-        "LA14_N": "AM14",
-        
-        "LA13_P": "AM13",
-        "LA13_N": "AN13",
-
-        "LA12_P": "AL10",
-        "LA12_N": "AM10",
-        
-        "LA11_P": "AT10",
-        "LA11_N": "AU10",
-        
-        "LA10_P": "AM12",
-        "LA10_N": "AN12",
-        
-        "LA09_P": "AN8",
-        "LA09_N": "AN7",
-        
-        "LA08_P": "AL9",
-        "LA08_N": "AM9",
-        
-        "LA07_P": "AK13",
-        "LA07_N": "AL12",
-        
-        "LA06_P": "AL8",
-        "LA06_N": "AL7",
-        
+        "LA00_CC_P": "AP9",
+        "LA00_CC_N": "AR9",
+        "LA01_CC_P": "AP8",
+        "LA01_CC_N": "AR8",
+		"LA01_P": "AP8",
+        "LA01_N": "AR8",
+        "LA02_P": "AH13",
+        "LA02_N": "AJ13",
+        "LA03_P": "AJ12",
+        "LA03_N": "AK12",
+        "LA04_P": "AG12",
+        "LA04_N": "AH12",
         "LA05_P": "AM8",
         "LA05_N": "AM7",
-        
-        "LA04_P": "L8",
-        "LA04_N": "K8",
-
-        "LA03_P": "L8",
-        "LA03_N": "K8",
-
-        "LA02_P": "L8",
-        "LA02_N": "K8",
-
-		"LA01_P": "L8",
-        "LA01_N": "K8",
-
-        "LA00_C_P": "L8",
-        "LA00_C_N": "K8",
-
-        "HA01_CC_P": "E16",
-        "HA01_CC_N": "D16",
-        "HA05_P": "J15",
-        "HA05_N": "J14",
-        "HA09_P": "F18",
-        "HA09_N": "F17",
-        "HA13_P": "B14",
-        "HA13_N": "A14",
-        "HA16_P": "A19",
-        "HA16_N": "A18",
-        "HA20_P": "C19",
-        "HA20_N": "B19",
-        "CLK1_M2C_P": "E25",
-        "CLK1_M2C_N": "D25",
-        "LA00_CC_P": "H11",
-        "HA03_P": "G15",
-        "HA03_N": "G14",
-        "HA07_P": "L19",
-        "HA07_N": "L18",
-        "HA11_P": "J19",
-        "HA11_N": "J18",
-        "HA14_P": "F15",
-        "HA14_N": "F14",
-        "HA18_P": "B17",
-        "HA18_N": "B16",
-        "HA22_P": "C18",
-        "HA22_N": "C17",
-        "GBTCLK1_M2C_P": "H6",
-        "GBTCLK1_M2C_N": "H5",
-        "GBTCLK0_M2C_P": "K6",
-        "GBTCLK0_M2C_N": "K5",
-        "PG_M2C": "L27",
-        "HA00_CC_P": "G17",
-        "HA00_CC_N": "G16",
-        "HA04_P": "G19",
-        "HA04_N": "F19",
-        "HA08_P": "K18",
-        "HA08_N": "K17",
-        "HA12_P": "K16",
-        "HA12_N": "J16",
-        "HA15_P": "D14",
-        "HA15_N": "C14",
-        "HA19_P": "D19",
-        "HA19_N": "D18",
-        "PRSNT_M2C_B": "H24",
-        "CLK0_M2C_P": "H12",
-        "CLK0_M2C_N": "G12",
-        "HA02_P": "H19",
-        "HA02_N": "H18",
-        "HA06_P": "L15",
-        "HA06_N": "K15",
-        "HA10_P": "H17",
-        "HA10_N": "H16",
-        "HA17_CC_P": "E18",
-        "HA17_CC_N": "E17",
-        "HA21_P": "E15",
-        "HA21_N": "D15",
-        "HA23_P": "B15",
-        "HA23_N": "A15",
+        "LA06_P": "AL8",
+        "LA06_N": "AL7",
+        "LA07_P": "AK13",
+        "LA07_N": "AL12",
+        "LA08_P": "AL9",
+        "LA08_N": "AM9",
+        "LA09_P": "AN8",
+        "LA09_N": "AN7",
+        "LA10_P": "AM12",
+        "LA10_N": "AN12",
+        "LA11_P": "AT10",
+        "LA11_N": "AU10",
+        "LA12_P": "AL10",
+        "LA12_N": "AM10",
+        "LA13_P": "AM13",
+        "LA13_N": "AN13",
+        "LA14_P": "AL14",
+        "LA14_N": "AM14",
+        "LA15_P": "AJ14",
+        "LA15_N": "AK14",
+        "LA16_P": "AR12",
+        "LA16_N": "AR11",
+        "LA17_P": "AN21",
+        "LA17_N": "AP21",
+        "LA18_P": "AM20",
+        "LA18_N": "AN20",
+		"LA19_P": "AU20",
+        "LA19_N": "AU19",
+		"LA20_P": "AR17",
+        "LA20_N": "AT17",
+        "LA21_P": "AL19",
+        "LA21_N": "AM19",
+        "LA22_P": "AR19",
+        "LA22_N": "AT19",
+        "LA23_P": "AM18",
+        "LA23_N": "AN18",
+        "LA24_P": "AL22",
+        "LA24_N": "AM22",
+        "LA25_P": "AL21",
+        "LA25_N": "AL20",
+        "LA26_P": "AR22",
+        "LA26_N": "AT22",
+        "LA27_P": "AR21",
+        "LA27_N": "AT21",
+        "LA28_P": "AJ18",
+        "LA28_N": "AK18",
+        "LA29_P": "AK22",
+        "LA29_N": "AK21",
+        "LA30_P": "AG20",
+        "LA30_N": "AH20",
+        "LA31_P": "AJ20",
+        "LA31_N": "AJ19",
+        "LA32_P": "AF20",
+        "LA32_N": "AF19",
+        "LA33_P": "AG18",
+        "LA33_N": "AH18",
         }
     ),
+
     ("LPC", {
+
         "GBTCLK0_M2C_P": "AA24",
         "GBTCLK0_M2C_N": "AA25",
         "LA01_CC_P": "W25",
